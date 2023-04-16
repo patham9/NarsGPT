7698a7d47c4a0a6c63b522db0cb7ae1c465edeb8
Patrick Hammer <patham9@gmail.com>
2023-04-16 13:25:34 +0200

    Update: NarsGPT.py: newer version which worked well and supports deduction properly

--- NarsGPT.py
+++ NarsGPT.py
@@ -1,7 +1,7 @@
 import sys
 import openai
 
-openai.api_key = "YOUR_KEY"
+openai.api_key = "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"
 
 #NAL truth functions
 def Truth_w2c(w):
@@ -22,16 +22,22 @@ def Truth_Revision(v1, v2):
     w1 = Truth_c2w(c1)
     w2 = Truth_c2w(c2)
     w = w1 + w2
+    if w == 0:
+        return (0.5, 0.0)
     return (min(1.0, (w1 * f1 + w2 * f2) / w), 
             min(0.99, max(max(Truth_w2c(w), c1), c2)))
+
+def Truth_Deduction(v1, v2):
+    (f1, c1) = v1
+    (f2, c2) = v2
+    return (f1*f2, f1*f2*c1*c2)
 #NAL truth functions end
 
 belief_prompt_start = """
 Commands:
-Claim(sentence) ... this relation is claimed to be true in the sentence
-NegatedClaim(sentence) ... the relation is claimed to be false in the sentence
-Deduce(sentence) ... the sentence which can be deduced from memory items.
-Remove(sentence) ... the duplicate sentence in memory which should be removed
+Input(sentence) ... this relation is true in the sentence
+NegatedInput(sentence) ... the relation is claimed to be false in the sentence
+Deduce(premise1,premise2,conclusion) ... the sentence which can be deduced from memory items.
 
 Capture the complete sentence meaning with code that calls the two functions.
 Please make sure that the word "not" is not included in your call, just use Claim and NegatedClaim.
@@ -45,30 +51,37 @@ belief_prompt_end = """
 The sentence: """
 
 question_prompt_start = """
-Please mention what content of Memory, whereby only certainty values close to 1 are "yes", and close to 0 "no".
+Mention concrete memory contents with certainty values.
+Use the minimum involved certainty value.
 
 Memory:
-
-The question: """
+"""
 
 question_prompt_end = """
 
 The question: """
 
-def promptgen(prompt_start, prompt_end):
-    prompt_memory = ""
+def workingmemory():
     selectedItems=[]
-    memorylist = list(memory.items())
+    memory_candidates_list = [x for x in list(memory.items()) if Truth_Expectation(x[1][2]) > 0.6]
     stmsize = 20
     #find 5 newest items:
-    memorylist.sort(key=lambda x: -x[1][0])
-    selectedItems += memorylist[0:int(stmsize/2)]
+    memory_candidates_list.sort(key=lambda x: -x[1][0])
+    selectedItems += memory_candidates_list[0:int(stmsize/2)]
     #find additional 5 useful items which were not already part of the newest
-    memorylist.sort(key=lambda x: -x[1][1])
+    memory_candidates_list.sort(key=lambda x: -x[1][1])
+    for x in selectedItems:
+        if x in memory_candidates_list:
+            memory_candidates_list.remove(x) #so we won't select it as it is already part of mem
     idx = 0
-    while len(selectedItems) < stmsize and idx < len(memorylist):
-        selectedItems.append(memorylist[idx])
+    while len(selectedItems) < stmsize and idx < len(memory_candidates_list):
+        selectedItems.append(memory_candidates_list[idx])
         idx+=1
+    return selectedItems
+
+def promptgen(prompt_start, prompt_end):
+    prompt_memory = ""
+    selectedItems = workingmemory()
     for i,x in enumerate(selectedItems):
         certainty = Truth_Expectation(x[1][2])
         prompt_memory += f"i={i}: {x[0]}. certainty={certainty}\n"
@@ -87,20 +100,20 @@ def process_commands(cmd, question):
             x = x[7:]
             truth = (0.0, 0.9)
         isDeduction = x.startswith("Deduce(")
-        isClaim = x.startswith("Claim(")
-        isRemoval = x.startswith("Remove(")
-        if (isDeduction or isClaim or isRemoval) and x.endswith(")"):
+        isInput = x.startswith("Input(")
+        if (isDeduction or isInput) and x.endswith(")"):
+            arg = x.split("(")[1].split(")")[0].replace('"','').replace("'","").replace(".", "").lower()
             if isDeduction:
-                truth = (1.0, 0.81) #TODO truth function by letting it refer to memory indices in deduce calls
-            arg = x.split("(")[1].split(")")[0].replace('"','').replace("'","")
-            if isRemoval:
-                if arg in memory:
-                    del memory[arg]
-                    print(x)
+                statements = [x.strip().replace(".", "") for x in arg.split(", ")]
+                if len(statements) != 3:
                     continue
+                if statements[0] not in memory or statements[1] not in memory:
+                    continue
+                truth = Truth_Deduction(memory[statements[0]][2], memory[statements[1]][2])
+                arg = statements[2] #the conclusion is to pbe put to memory
             if arg in memory and isDeduction:
                 continue #deduction cannot revise currently as stamp is yet to find a solution fo
-            if isDeduction or isClaim:
+            if isDeduction or isInput:
                 if isNegated:
                     print("Neg"+x)
                 else:
@@ -116,8 +129,14 @@ while True:
         inp = input().rstrip("\n")
     except:
         exit(0)
-    if inp.startswith("*mem"):
-        print(memory)
+    if inp.startswith("*ltm"):
+        for x in memory.items():
+            print(x)
+        continue
+    if inp.startswith("*wm"):
+        wm = workingmemory()
+        for x in wm:
+            print(x)
         continue
     question = False
     if inp.endswith("?"):
@@ -125,15 +144,14 @@ while True:
         inp=inp[:-1]+" according to Memory?"
         send_prompt = promptgen(question_prompt_start, question_prompt_end) + inp
     else:
-        send_prompt = promptgen(belief_prompt_start, belief_prompt_end) + inp
+        send_prompt = promptgen(belief_prompt_start, belief_prompt_end) + inp + ". And deduce what you can!"
         currentTime += 1
     #print("vvvvSTART PROMPT", send_prompt, "\n^^^^END PROMPT")
     response = openai.ChatCompletion.create(
     model='gpt-3.5-turbo',
       messages=[
         {"role": "user", "content": send_prompt}],
-    max_tokens=50,
+    max_tokens=100,
     temperature=0)
     commands = response['choices'][0]['message']['content'].split("\n")
     process_commands(commands, question)
-@patham9
