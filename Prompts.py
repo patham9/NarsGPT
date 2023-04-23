2a3ea566e55469e6ae4a39d7fe824c1867964d34
Patrick Hammer <patham9@gmail.com>
2023-04-23 08:45:04 +0200

    Update: word count should be the same as in first case

--- Prompts.py
+++ Prompts.py
@@ -48,4 +48,4 @@ Memory:
 
 Prompts_question_end = " according to Memory? Answer in a probabilistic sense and within 15 words based on memory content only."
 #If it should be allowed to consider GPT's 'weight-based' knowledge too, set IncludeGPTKnowledge=True, then the following is utilized:
-Prompts_question_end_alternative = "? Answer in a probabilistic sense and within 10 words."
+Prompts_question_end_alternative = "? Answer in a probabilistic sense and within 15 words."
