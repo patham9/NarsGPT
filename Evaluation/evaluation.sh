842f0b2c17b0ff089c483679c22d8bc49ed9b7b1
Patrick Hammer <patham9@gmail.com>
2023-04-30 01:14:53 +0200

    Update: ability to compare with previous runs pointing out differences in answers and differences in correctness judgment

--- Evaluation/evaluation.sh
+++ Evaluation/evaluation.sh
@@ -1,3 +1,7 @@
+move Correct.json CorrectBefore.json
+move Incorrect.json IncorrectBefore.json
 python3 1_GenerateTestOutput.py API_KEY=YOUR_KEY < INT_Inf_benchmarkTest.csv
 python3 2_EvaluateTestOutput.py API_KEY=YOUR_KEY
+python3 3_CheckDifferencesToLastRun.py > differences.txt
+cat differences.txt
 cat Scores.json
