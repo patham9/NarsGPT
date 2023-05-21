643b5028548389cfd07d7b0fc7e0f3f00725c431
Patrick Hammer <patham9@gmail.com>
2023-05-21 20:16:06 +0200

    Update: babI16 scripts

---
+++ Evaluation_babI_qa16/1_GenerateTestOutput.py
@@ -0,0 +1,53 @@
+import sys
+import os
+cwd = os.getcwd()
+sys.path.append(cwd + "/../")
+os.chdir(cwd + "/../")
+from NarsGPT import *
+os.chdir(cwd)
+import json
+
+lines = ""
+with open("/home/tc/babI/tasks_1-20_v1-2/en-valid/qa16_train.txt") as f:
+    lines = f.read().split("\n")
+
+lastnum = -1
+examples = []
+example_cur = []
+for line in lines:
+    if line.strip() == "":
+        continue
+    words = line.split(" ")
+    number, text = (int(words[0]), " ".join(words[1:]))
+    if number < lastnum:
+        examples.append(example_cur)
+        example_cur = []
+    example_cur.append(text)
+    lastnum = number
+examples.append(example_cur)
+
+def question_and_expected_output(questionline):
+    splitted = questionline.split("\t")
+    return [splitted[0], splitted[1]]
+
+examples = [[" ".join(example[0:-1])] + question_and_expected_output(example[-1]) for example in examples]
+
+Line_Input_Output_ExpectedOutput = []
+ExampleID = 1
+for example in examples:
+    BeliefInput, QuestionInput, expectedOutput = example
+    expectedOutput = expectedOutput.strip()
+    outputFromBeliefInput = NarsGPT_AddInput(BeliefInput)
+    actualOutput = NarsGPT_AddInput(QuestionInput)
+    Dic = {"ExampleID": ExampleID, "Input": BeliefInput + " " + QuestionInput, "OutputFromBeliefInput": outputFromBeliefInput,"actualOutput": actualOutput, "expectedOutput": expectedOutput}
+    Line_Input_Output_ExpectedOutput.append(Dic)
+    for k in Dic:
+        print(k+":", Dic[k])
+    print("\n")
+    filename = "TestOutput.json"
+    with open(filename, 'w') as f:
+        json.dump(Line_Input_Output_ExpectedOutput, f)
+    ExampleID += 1
+    NarsGPT_AddInput("*reset")
+
+
---
+++ Evaluation_babI_qa16/2_EvaluateTestOutput.py
@@ -0,0 +1,61 @@
+import json
+import openai
+import time
+import sys
+
+for x in sys.argv:
+    if x.startswith("API_KEY="):
+        openai.api_key = x.split("API_KEY=")[1]
+
+with open("TestOutput_.json") as json_file:
+    ListOfDicts = json.load(json_file)
+
+# {"Line": Line, "Input": Input, "actualOutput": actualOutput, "expectedOutput": expectedOutput}
+Questions = []
+for D in ListOfDicts:
+    Line = D["ExampleID"]
+    Input = D["Input"]
+    actualOutput = D["actualOutput"]
+    expectedOutput = D["expectedOutput"]
+    if expectedOutput != "" and len(D["expectedOutput"].split("/")) <= 1: #no questions about Aigo's innate time handling
+        Questions.append(D)
+    
+PROMPT = """Does the actual output contain the asked information answered in the expected output?
+The question: _QUESTION_
+The actual output: _ACTUAL_OUTPUT_
+The expected output: _EXPECTED_OUTPUT_
+Please answer yes/no only!"""
+
+Correct = []
+Incorrect = []
+for D in Questions:
+    Line = D["ExampleID"]
+    Input = D["Input"].split(". ")[-1]
+    actualOutput = D["actualOutput"]
+    expectedOutput = D["expectedOutput"]
+    send_prompt = PROMPT.replace("_QUESTION_", Input).replace("_ACTUAL_OUTPUT_",actualOutput).replace("_EXPECTED_OUTPUT_",expectedOutput)
+    print(send_prompt)
+    while True:
+        try:
+            response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=[ {"role": "user", "content": send_prompt}], max_tokens=200, temperature=0)
+            ret = response['choices'][0]['message']['content']
+        except:
+            print("Error: API call failed, will try repeating it in 10 seconds!")
+            time.sleep(10) #wait 10 seconds
+            continue
+        break
+    YES = "yes" in ret.lower()
+    D["Correct"] = YES
+    print("Correct?", YES)
+    if YES:
+        Correct.append(D)
+    else:
+        Incorrect.append(D)
+    scores = {"Correct": len(Correct), "Incorrect": len(Incorrect), "Ratio" : float(len(Correct)) / float(len(Correct)+len(Incorrect))}
+    print("So far:", scores)
+    with open("Correct.json", 'w') as f:
+        json.dump(Correct, f)
+    with open("Incorrect.json", 'w') as f:
+        json.dump(Incorrect, f)
+    with open("Scores.json", 'w') as f:
+        json.dump(scores, f)
