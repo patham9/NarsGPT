10cc4d36c7e500d17be0fb3119c5881c98c15656
Patrick Hammer <patham9@gmail.com>
2023-06-03 18:04:46 +0200

    Update: feeed question back to next input so that short inputs become possible

--- Demo1_LearnAboutUser.py
+++ Demo1_LearnAboutUser.py
@@ -1,13 +1,21 @@
 import NarsGPT as NAR
 
+UseLastQuestion = True
+lastquestion = ""
 def AddInput(inp):
+    if not inp.endswith("?") and not inp.startswith("*") and UseLastQuestion:
+        inp = lastquestion + " " + inp
     ret = NAR.AddInput(inp, Print=False, PrintInputSentenceOverride=True, PrintInputSentenceOverrideValue=True)
     if inp.endswith("?"):
         print(ret["GPT_Answer"])
 
 def RaiseQuestion():
+    global lastquestion
     ret = NAR.AddInput("Raise a question about me, not addressed by any existing memory item?", Print=False, PrintInputSentenceOverride=True, PrintInputSentenceOverrideValue=False)
-    print(ret["GPT_Answer"].split("?")[0] + "?")
+    ret["GPT_Answer"] = ret["GPT_Answer"].split("?")[0] + "?"
+    print(ret["GPT_Answer"])
+    NAR.I_You_Exchange(ret)
+    lastquestion = ret["GPT_Answer"]
 RaiseQuestion()
 
 while True:
